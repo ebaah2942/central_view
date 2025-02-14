8f2ff3d2ef7d915a58586ed6cba4681099b19874
Enoch Baah <ebaah2942@gmail.com>
2025-02-14 12:43:11 +0000

    Added single image

--- hotel_website/hotel_website/settings.py
+++ hotel_website/hotel_website/settings.py
@@ -159,8 +159,9 @@ STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = "myacvhbucket"
-AWS_S3_REGION_NAME = "us-east-1"  # Change this to your region
+AWS_S3_REGION_NAME = "eu-north-1"  # Change this to your region
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+AWS_DEFAULT_ACL = "public-read"
 
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
